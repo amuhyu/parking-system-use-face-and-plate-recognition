92854dd48d95160b3ed56d98a28ed29143b88e19
amuhyu <andipucang250501@gmail.com>
2023-08-04 10:13:01 +0800

    First Commit

---
+++ src/testYolo.py
@@ -0,0 +1,20 @@
+import cv2
+import torch
+import numpy as np
+import easyocr
+
+path = 'best.pt'
+
+plate = torch.hub.load('ultralytics/yolov5', 'custom',path, force_reload=True)
+cap=cv2.VideoCapture(0)
+while True:
+    ret,frame=cap.read()
+    frame=cv2.resize(frame,(640,480))
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    results=plate(frame)
+    frame=np.squeeze(results.render())  
+    cv2.imshow("TESTING",frame)
+    if cv2.waitKey(1)&0xFF==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
---
+++ src/testYoloANPR.py
@@ -0,0 +1,64 @@
+import cv2
+import torch
+import numpy as np
+import easyocr
+import csv
+
+# Load YOLOv5 model
+path = 'best.pt'
+
+model = torch.hub.load('ultralytics/yolov5', 'custom',path, force_reload=True)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device).eval()
+
+# Initialize the OCR reader
+import easyocr
+reader = easyocr.Reader(['en'], gpu=False)
+
+def detect_and_crop_webcam(confidence_threshold=0.5):
+    cap = cv2.VideoCapture(0)  # Menggunakan webcam dengan nomor indeks 0
+
+    csv_file = open('ocr_results.csv', mode='w', newline='')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Label', 'OCR Text'])
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Perform detection
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = model(img_rgb, size=640)
+        detected_objects = results.pandas().xyxy[0]
+
+        # Crop and display detected objects
+        for _, obj in detected_objects.iterrows():
+            label = obj['name']
+            conf = obj['confidence']
+            if conf > confidence_threshold:
+                x1, y1, x2, y2 = int(obj['xmin']), int(obj['ymin']), int(obj['xmax']), int(obj['ymax'])
+                cropped_img = frame[y1:y2, x1:x2]
+
+                # Perform OCR on cropped image
+                gray_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+                blur_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
+                # results = reader.readtext(gray_img, detail = 0, paragraph = True)
+                results = reader.readtext(blur_img, detail = 0, paragraph= True)
+                print(results)
+
+                cv2.imshow(label, cropped_img)
+
+                csv_writer.writerow([label, results])
+        
+        cv2.imshow('Webcam', frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:  # Tekan 'Esc' untuk keluar
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    cv2.namedWindow('Webcam', cv2.WINDOW_NORMAL)
+    detect_and_crop_webcam()
